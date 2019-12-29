cbbe54394dec5025faab4d876de1b0b53b44b4d9
Sayyid Maulana Khakul Yakin <sayyid.yakin@students.amikom.ac.id>
2019-12-29 08:17:16 +0700

    Initial Commit

---
+++ ZooApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZooApp
+//
+//  Created by sayyid maulana khakul yakin on 29/12/19.
+//  Copyright © 2019 sayyid maulana khakul yakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
