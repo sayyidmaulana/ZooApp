24122f788f75127672a0e7931cd38643c44d9dae
Sayyid Maulana Khakul Yakin <sayyid.yakin@students.amikom.ac.id>
2019-12-29 10:17:20 +0700

    feature(AnimalDetail) List of detail any animals

---
+++ ZooApp/DetailController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailController.swift
+//  ZooApp
+//
+//  Created by sayyid maulana khakul yakin on 29/12/19.
+//  Copyright © 2019 sayyid maulana khakul yakin. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ZooApp/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  ZooApp
+//
+//  Created by sayyid maulana khakul yakin on 29/12/19.
+//  Copyright © 2019 sayyid maulana khakul yakin. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
